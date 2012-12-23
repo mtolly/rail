a8b442a2f9de40c12639ec018e33dd99e9307cc9
Michael Tolly <tolly@wisc.edu>
2012-12-22 23:02:16 -0600

    added c output to main program

--- Main.hs
+++ Main.hs
@@ -2,8 +2,18 @@ module Main where
 
 import System.Environment (getArgs)
 import System.IO (hPutStrLn, stderr)
+import Language.Rail.Base
 import Language.Rail.Run
+import Language.Rail.C
+import Language.C.Syntax.AST
+import Language.C.Pretty
 
 main = getArgs >>= \argv -> case argv of
-  f : _ -> readFile f >>= runMemory . compile
+  [fin] -> readFile fin >>= runMemory . compile
+  [fin, fout] -> do
+    s <- readFile fin
+    let tunit = CTranslUnit (makeProgram $ getFunctions s) unn
+    head <- header
+    let output = head ++ show (pretty $ tunit)
+    writeFile fout output
   _ -> hPutStrLn stderr "usage: hrail <file>"
