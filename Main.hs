5866f95bc21ae56f5b341b9c1ce1529baf1f6bd6
Michael Tolly <tolly@wisc.edu>
2013-03-06 15:39:52 -0600

    added rail->rail cleanup option to executable

--- Main.hs
+++ Main.hs
@@ -4,14 +4,20 @@ import System.Environment (getArgs)
 import System.IO (hPutStrLn, stderr)
 import Language.Rail.Parse
 import Language.Rail.Run
-import Language.Rail.C
+import qualified Language.Rail.C as C
+import qualified Language.Rail.Generate as Gen
 
 main :: IO ()
 main = getArgs >>= \argv -> case argv of
   [fin] -> readFile fin >>= runMemory . compile
-  [fin, fout] -> do
-    s <- readFile fin
-    writeFile fout $ makeFile $ getFunctions s
+  ["-c", fin, fout] ->
+    readFile fin >>= writeFile fout . C.makeFile . getFunctions
+  ["-r", fin, fout] ->
+    readFile fin >>= writeFile fout . Gen.program . getFunctions
   _ -> mapM_ (hPutStrLn stderr)
-    [ "usage: hrail file-in.rail            (directly interpret)"
-    , "       hrail file-in.rail file-out.c (generate C code)" ]
+    [ "usage: hrail file-in.rail                  (directly interpret)"
+    , "       hrail -c file-in.rail file-out.c    (generate C code)"
+    , "       hrail -r file-in.rail file-out.rail (clean up Rail code)"
+    ]
+
+
